2c764f8b0e7073ec0129129c0b4a104237e8fab1
Steve Thornton <technission@outlook.com>
2024-02-29 12:52:41 -0600

    Updated code for analytics

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -2,6 +2,8 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const TAB_DATA = [
   {
