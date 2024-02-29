b2319319dd983b7f4a899b025d0e5bb89a6755e7
Steve Thornton <technission@outlook.com>
2024-02-29 12:43:02 -0600

    Added Speed Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
